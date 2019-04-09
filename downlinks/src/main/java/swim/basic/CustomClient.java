cec614df0f5dbaccafb15a354a680bebd718d32b
brohitbrose <rohit.bose95@gmail.com>
2019-04-09 16:59:09 -0700

    Apply css class to the correct entry

---
+++ downlinks/src/main/java/swim/basic/CustomClient.java
@@ -0,0 +1,41 @@
+package swim.basic;
+
+import swim.api.downlink.MapDownlink;
+import swim.client.ClientRuntime;
+import swim.structure.Form;
+import swim.structure.Text;
+import swim.structure.Value;
+
+class CustomClient {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    ClientRuntime swimClient = new ClientRuntime();
+    swimClient.start();
+
+    final String hostUri = "warp://localhost:9001";
+    final String nodeUriPrefix = "/unit/";
+
+    // Write-only downlink; note keepLinked is false
+    final MapDownlink<String, Integer> link = swimClient.downlinkMap()
+        .keyForm(Form.forString()).valueForm(Form.forInteger())
+        .hostUri(hostUri).nodeUri(nodeUriPrefix+"0").laneUri("shoppingCart")
+        .keepLinked(false)
+        .open();
+    link.put("FromClientLink", 25);
+
+    Thread.sleep(1000);
+    // Hereafter we will strictly use commands, so link is safe to close
+    link.close();
+
+    final String[] items = {"bat", "cat", "rat"};
+    for (int i = 0; i < 50; i++) {
+      // randomly add an item from `items` to the shopping carts of /unit/0, /unit/1, /unit/2 via commands
+      swimClient.command(hostUri, nodeUriPrefix+(i%3), "addItem", Text.from(items[(int) (Math.random() * 3)]));
+    }
+
+    System.out.println("Will shut down client in 2 seconds");
+    Thread.sleep(2000);
+    swimClient.stop();
+  }
+}
---
+++ downlinks/src/main/java/swim/basic/UnitAgent.java
@@ -0,0 +1,36 @@
+package swim.basic;
+
+import swim.api.SwimLane;
+import swim.api.agent.AbstractAgent;
+import swim.api.lane.CommandLane;
+import swim.api.lane.MapLane;
+import swim.recon.Recon;
+import swim.structure.Record;
+import swim.structure.Text;
+import swim.structure.Value;
+import swim.uri.Uri;
+
+public class UnitAgent extends AbstractAgent {
+
+  @SwimLane("shoppingCart")
+  MapLane<String, Integer> shoppingCart = this.<String, Integer>mapLane()
+      .didUpdate((key, newValue, oldValue) -> {
+        logMessage(key + " count changed to " + newValue + " from " + oldValue);
+      });
+
+  @SwimLane("addItem")
+  CommandLane<String> publish = this.<String>commandLane()
+      .onCommand(msg -> {
+        final int n = this.shoppingCart.getOrDefault(msg, 0) + 1;
+        this.shoppingCart.put(msg, n);
+      });
+
+  private void logMessage(Object msg) {
+    System.out.println(nodeUri() + ": " + msg);
+  }
+
+  @Override
+  public void didStart() {
+    command("/listener", "triggerListen", Text.from(nodeUri().toString()));
+  }
+}
