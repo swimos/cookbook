3549f53cd61449e086e769ccfdc9d7ff892789a8
Rohit Bose <rohit.bose95@gmail.com>
2019-07-01 12:37:28 -0700

    Copyrights

--- value_lanes/src/main/java/swim/basic/BasicPlane.java
+++ value_lanes/src/main/java/swim/basic/BasicPlane.java
@@ -1,3 +1,17 @@
+// Copyright 2015-2019 SWIM.AI inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package swim.basic;
 
 import java.io.IOException;
@@ -14,7 +28,7 @@ public class BasicPlane extends AbstractPlane {
   @SwimRoute("/unit/:id")
   final AgentType<UnitAgent> unitAgentType = agentClass(UnitAgent.class);
 
-  public static void main(String[] args) throws IOException, InterruptedException {
+  public static void main(String[] args) throws IOException {
     final ServerContext server = ServerLoader.load(BasicPlane.class.getModule()).serverContext();
     server.start();
     final PlaneContext plane = server.getPlane("basic").planeContext();
