f53e851f0ce0435e4bd9946d8ae663f2316301ce
DobromirM <mr.d.marinov@gmail.com>
2019-09-26 18:46:43 +0100

    * Removed unused imports

--- value_lanes/src/main/java/swim/basic/UnitAgent.java
+++ value_lanes/src/main/java/swim/basic/UnitAgent.java
@@ -18,11 +18,6 @@ import swim.api.SwimLane;
 import swim.api.agent.AbstractAgent;
 import swim.api.lane.CommandLane;
 import swim.api.lane.ValueLane;
-import swim.recon.Recon;
-import swim.structure.Record;
-import swim.structure.Text;
-import swim.structure.Value;
-import swim.uri.Uri;
 
 public class UnitAgent extends AbstractAgent {
 
