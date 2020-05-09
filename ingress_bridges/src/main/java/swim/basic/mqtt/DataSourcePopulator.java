62b58efd1decfc32910cd9f3a9f2ab5a56a26a2c
Christos Vasilakis <christos.vasilakis@arm.com>
2020-05-09 21:24:25 +0100

    iot.eclipse is replaced with mqtt.eclipse since the former dns name is discontinued.

--- ingress_bridges/src/main/java/swim/basic/mqtt/DataSourcePopulator.java
+++ ingress_bridges/src/main/java/swim/basic/mqtt/DataSourcePopulator.java
@@ -33,7 +33,7 @@ public class DataSourcePopulator {
   }
 
   public static void main(String[] args) throws MqttException {
-    final DataSourcePopulator pop = new DataSourcePopulator("tcp://iot.eclipse.org:1883");
+    final DataSourcePopulator pop = new DataSourcePopulator("tcp://mqtt.eclipse.org:1883");
     pop.populate();
   }
 
