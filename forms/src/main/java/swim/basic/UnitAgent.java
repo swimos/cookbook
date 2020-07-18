9f66756b0dbe866e900debfa388b6b386875eb41
Crista2019 <crista2019@gmail.com>
2020-07-17 17:05:12 -0700

    add callback methods to lanes

--- forms/src/main/java/swim/basic/UnitAgent.java
+++ forms/src/main/java/swim/basic/UnitAgent.java
@@ -1,26 +1,32 @@
 public class UnitAgent {
 	
 	@SwimLane("foo")
-	public ValueLane<FooType> foo;
-	// TODO: where in a swim agent class do you put methods such as didSet() ?
-	// TODO: didSet(newValue, oldValue) callback prints newValue
+	public ValueLane<FooType> foo = this.<FooType>valueLane()
+			.didSet(newValue, oldValue) -> {
+				System.out.println(newValue);
+			});
 	
 	@SwimLane("addFoo")
-  public CommandLane<FooType> addFoo = this.<FooType>commandLane().onCommand((FooType value) -> {
-    foo.set(value);
-  }
+  public CommandLane<FooType> addFoo = this.<FooType>commandLane()
+  	.onCommand((FooType value) -> {
+  		foo.set(value);
+  	});
 
   @SwimLane("bar")
-  public ValueLane<BarType> bar;
-  // TODO: (newValue, oldValue) callback prints newValue
+  public ValueLane<BarType> bar = this.<BarType>valueLane()
+  		.didSet(newValue, oldValue) -> {
+  			System.out.println(newValue);
+  		});
   
   @SwimLane("addBar")
-  public CommandLane<FooType> addBar = this.<BarType>commandLane().onCommand((BarType value) -> {
-    bar.set(value);
-  }
+  public CommandLane<FooType> addBar = this.<BarType>commandLane()
+  	.onCommand((BarType value) -> {
+  		bar.set(value);
+  	});
 	
   @SwimLane("addValue")
-  public CommandLane<Value> addValue = this<Value>commandLane().onCommand((Value value)) -> {
-  	console.log(value);
-  }
+  public CommandLane<Value> addValue = this.<Value>commandLane()
+  	.onCommand((Value value) -> {
+  		System.out.println(value);
+  	});
 }
