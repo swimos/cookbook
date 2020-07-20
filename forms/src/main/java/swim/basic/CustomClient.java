0bbcad4da3317ee865ed8c3c63a42f36aa4ea804
Crista2019 <crista2019@gmail.com>
2020-07-20 16:49:16 -0700

    call addValue lane for both fooType and barType

--- forms/src/main/java/swim/basic/CustomClient.java
+++ forms/src/main/java/swim/basic/CustomClient.java
@@ -14,20 +14,23 @@ class CustomClient {
     final String barNodeUri = "/unit/bar";
 
     Value fooVal = Record.create(3).attr("foo").slot("i", 5).slot("s", "potato");
+
     BarType barObj = new BarType(1, "two", 3);
     Value barVal = (Value) BarType.form().mold(barObj);
 
     for (int i = 0; i < 10; i++) {
       swimClient.command(hostUri, fooNodeUri, "addFoo", fooVal);
       swimClient.command(hostUri, barNodeUri, "addBar", barVal);
+
+      swimClient.command(hostUri, fooNodeUri, "addValue", fooVal);
+      swimClient.command(hostUri, barNodeUri, "addValue", barVal);
+
       Thread.sleep(5000 * i);
     }
     System.out.println("Will shut down client");
     swimClient.stop();
   }
 }
-
-// TODO: replace the above boilerplate code with relevant code that is cookbook specific
 
 // TODO: from git issue
 //Commands the addFoo lane of UnitAgent with some Record that is compatible with FooType. With the example, and assuming the class was annotated with @Tag("foo"), such a Record might look like Record.create(3).attr("foo").slot("i", 5).slot("s", "potato").
