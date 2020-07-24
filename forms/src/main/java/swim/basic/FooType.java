18b49c4873222b0637e24e1f4cd99c8e606828a3
Crista2019 <crista2019@gmail.com>
2020-07-24 11:01:24 -0700

    add @Member annotation to class vars

--- forms/src/main/java/swim/basic/FooType.java
+++ forms/src/main/java/swim/basic/FooType.java
@@ -3,6 +3,7 @@ package swim.basic;
 import swim.recon.Recon;
 import swim.structure.Form;
 import swim.structure.Kind;
+import swim.structure.Member;
 import swim.structure.Tag;
 
 // Fundamentally a simple Java class, with a small number of fields of any simple type
@@ -10,38 +11,40 @@ import swim.structure.Tag;
 
 @Tag("fooType")
 public class FooType {
-	private int i = 0;
-	private String s = "";
-	
-	public FooType() {
-	}
-	
-	public FooType(int i, String s) {
-		this.i = i;
-		this.s = s;
-	}
-	
-	public int getNumber() {
-		return i;
-	}
-	
-	public String getString() {
-		return s;
-	}
+  @Member("int")
+  private int i = 0;
+  @Member("string")
+  private String s = "";
 
-	//@Kind-annotated static field and static accessor method
-	@Kind
-	private static Form<FooType> form;
-	
-	public static Form<FooType> form() {
-		if(form == null) {
-			form = Form.forClass(FooType.class);
-		}
-		return form;
-	}
+  public FooType() {
+  }
 
-	@Override
-	public String toString() {
-		return Recon.toString(form().mold(this));
-	}
+  public FooType(int i, String s) {
+    this.i = i;
+    this.s = s;
+  }
+
+  public int getNumber() {
+    return i;
+  }
+
+  public String getString() {
+    return s;
+  }
+
+  //@Kind-annotated static field and static accessor method
+  @Kind
+  private static Form<FooType> form;
+
+  public static Form<FooType> form() {
+    if (form == null) {
+      form = Form.forClass(FooType.class);
+    }
+    return form;
+  }
+
+  @Override
+  public String toString() {
+    return Recon.toString(form().mold(this));
+  }
 }
