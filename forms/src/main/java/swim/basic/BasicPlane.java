1e234b7bb9c70a8fb86d8b7d9c64df876e3a8149
Crista2019 <crista2019@gmail.com>
2020-07-24 09:51:54 -0700

    exercise basic pojo transformations

--- forms/src/main/java/swim/basic/BasicPlane.java
+++ forms/src/main/java/swim/basic/BasicPlane.java
@@ -7,6 +7,7 @@ import swim.api.plane.AbstractPlane;
 import swim.api.space.Space;
 import swim.kernel.Kernel;
 import swim.server.ServerLoader;
+import swim.structure.Record;
 import swim.structure.Value;
 
 public class BasicPlane extends AbstractPlane {
@@ -14,7 +15,28 @@ public class BasicPlane extends AbstractPlane {
   @SwimRoute("/unit/:id")
   AgentRoute<UnitAgent> unitAgentType;
 
-  public static void main(String[] args) {
+  public static void pojoTransformations() {
+    // FooType instance fooType
+    FooType fooType = new FooType(7, "cat");
+
+    // BarType instance barType
+    BarType barType = new BarType(4, "dog", 2);
+
+    Value fooVal = (Value) FooType.form().mold(fooType);
+    Value barVal = (Value) BarType.form().mold(barType);
+
+    System.out.println(FooType.form().cast(fooVal));
+    System.out.println(BarType.form().cast(barVal));
+
+    // The following two statements return null. Why do you think that is?
+    System.out.println(FooType.form().cast(barVal));
+    System.out.println(BarType.form().cast(fooVal)); // TODO: this isn't null actually, strangely it creates an empty BarType object
+
+    // Consider why the following would fail to compile:
+    // FooType.form().mold(barType);
+  }
+
+  public static void runServer() {
     final Kernel kernel = ServerLoader.loadServer();
     final Space space = kernel.getSpace("basic");
 
@@ -22,5 +44,16 @@ public class BasicPlane extends AbstractPlane {
     System.out.println("Running Basic server...");
     space.command("/unit/foo", "wakeup", Value.absent());
     kernel.run();
+  }
+
+  public static void main(String[] args) {
+    // Switch the flag to tue to run the server only once you understand what happens in pojoTransformations()
+    Boolean willRunServer = false;
+
+    pojoTransformations();
+
+    if (willRunServer) {
+      runServer();
+    }
   }
 }
