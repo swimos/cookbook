6793ffecfb8a0937d706a27708a875c8c9f35b36
ajay-gov <ajay.gov@protonmail.com>
2023-04-17 11:17:06 -0700

    Fix the client and server downlinks to use the right port

--- join_map_lanes/src/main/java/swim/basic/BasicPlane.java
+++ join_map_lanes/src/main/java/swim/basic/BasicPlane.java
@@ -1,11 +1,11 @@
 package swim.basic;
 
-import swim.actor.ActorSpaceDef;
+import swim.actor.ActorSpace;
 import swim.api.downlink.MapDownlink;
 import swim.api.plane.AbstractPlane;
+import swim.api.space.Space;
 import swim.kernel.Kernel;
 import swim.server.ServerLoader;
-import swim.service.web.WebServiceDef;
 
 /**
  * The complimentary code as part of the <a href="https://swimos.org/tutorials/join-map-lanes/">Join Map Lane</a> cookbook.
@@ -18,13 +18,13 @@ import swim.service.web.WebServiceDef;
  */
 public class BasicPlane extends AbstractPlane {
 
-  static final String HOST_URI = "warp://localhost:53556";
+  static final String HOST_URI = "warp://localhost:9001";
 
   /**
    * Returns a downlink map for a given plane and node URI using the 'state' lane URI
    */
-  private static MapDownlink<String, Integer> initDownlink(BasicPlane plane, String uri) {
-    return plane.downlinkMap()
+  private static MapDownlink<String, Integer> initDownlink(Space space, String uri) {
+    return space.downlinkMap()
             .keyClass(String.class)
             .valueClass(Integer.class)
             .hostUri(HOST_URI)
@@ -35,16 +35,12 @@ public class BasicPlane extends AbstractPlane {
 
   public static void main(String[] args) throws InterruptedException {
     final Kernel kernel = ServerLoader.loadServer();
-    // Open a space to work with. This is instead of defining a server.recon file. Which one could also do
-    final BasicPlane plane = kernel.openSpace(ActorSpaceDef.fromName("test"))
-            .openPlane("test", BasicPlane.class);
-
-    kernel.openService(WebServiceDef.standard().port(53556).spaceName("test"));
+    final ActorSpace space = (ActorSpace) kernel.getSpace("basic");
     kernel.start();
 
-    final MapDownlink<String, Integer> californiaDownlink = initDownlink(plane, "/state/california");
-    final MapDownlink<String, Integer> texasDownlink = initDownlink(plane, "/state/texas");
-    final MapDownlink<String, Integer> floridaDownlink = initDownlink(plane, "/state/florida");
+    final MapDownlink<String, Integer> californiaDownlink = initDownlink(space, "/state/california");
+    final MapDownlink<String, Integer> texasDownlink = initDownlink(space, "/state/texas");
+    final MapDownlink<String, Integer> floridaDownlink = initDownlink(space, "/state/florida");
 
     // A preferred approach is to use countdown latches but for brevity a sleep suffices
     Thread.sleep(1000);
