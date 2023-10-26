6c1d7413ab1ac1530c10e4fd0d5db4d6a5077b71
Alex Piggott <piggottdev@gmail.com>
2023-10-26 18:24:54 +0100

    Updated the vehicle agent to a value lane

--- mongodb_ingestion/src/main/java/swim/vehicle/VehicleAgent.java
+++ mongodb_ingestion/src/main/java/swim/vehicle/VehicleAgent.java
@@ -3,17 +3,17 @@ package swim.vehicle;
 import swim.api.SwimLane;
 import swim.api.agent.AbstractAgent;
 import swim.api.lane.CommandLane;
-import swim.api.lane.MapLane;
+import swim.api.lane.ValueLane;
 import swim.structure.Value;
 
 public class VehicleAgent extends AbstractAgent {
 
   @SwimLane("addMessage")
   CommandLane<Value> addMessage = this.<Value>commandLane()
-      .onCommand(v -> this.vehicles.put(v.get("id").longValue(), v));
+      .onCommand(v -> this.metadata.set(v));
 
-  @SwimLane("vehicles")
-  MapLane<Long, Value> vehicles = this.<Long, Value>mapLane()
-      .didUpdate((k, n, o) -> System.out.println(nodeUri() + ": received " + n));
+  @SwimLane("metadata")
+  ValueLane<Value> metadata = this.<Value>valueLane()
+      .didSet((n, o) -> System.out.println(nodeUri() + ": received " + n));
 
 }
